2df2051d598f30fbf8f5cec54183d895226fbd37
NkwaTambe <nkwajudetambe@gmail.com>
2025-07-23 13:56:53 +0100

    fix: update pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "./",
   plugins: [
     react(),
     VitePWA({
@@ -10,19 +11,18 @@ export default defineConfig({
       manifest: {
         name: "ReportHub",
         short_name: "EventRecorder",
-        start_url: "/test/",
-        scope: "/test/",
+        start_url: "/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#007bff",
         icons: [
           {
-            src: "/test/download1.png",
+            src: "/download1.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/test/download2.png",
+            src: "/download2.png",
             sizes: "512x512",
             type: "image/png",
           },
