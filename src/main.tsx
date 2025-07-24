f39458a8307c76e621aa0182f11ae76ba2a45779
NkwaTambe <nkwajudetambe@gmail.com>
2025-07-24 08:38:06 +0100

    remove hashroute

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,11 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter>
       <I18nextProvider i18n={i18n}>
         <App />
       </I18nextProvider>
-    </HashRouter>
   </StrictMode>,
 );
